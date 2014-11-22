6cf8128a5355c1fde76e8a3a959a2664fb295d8c
Sara Cavic <saracavic@gmail.com>
2014-11-21 21:58:57 -0500

    Add bootstrap to layout

--- spec/features/layout_spec.rb
+++ spec/features/layout_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe 'Application layout' do
   it 'shows the application name in an h1' do
     visit '/'
-    expect(page).to have_css 'h1', text: 'Video Store'
+    expect(page).to have_css 'a', text: 'Video Store'
   end
 end
